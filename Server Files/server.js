f469193351619cf570f14fdfa52decd7a3b2b10e
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2020-12-12 20:00:45 -0600

    -Added nav menu to top, in basic form. -Added html pages for character screen and inventory management.

--- Server Files/server.js
+++ Server Files/server.js
@@ -8,6 +8,7 @@ const root = path.join(__dirname,"..\\");
 const webpageRoot = path.join(__dirname,"..\\","Webpage Files");
 const serverRoot = path.join(__dirname,"..\\","Server Files");
 const bungieAuthURL = "https://www.bungie.net/en/OAuth/Authorize";
+const bungieTokURL = "https://www.bungie.net/platform/app/oauth/token/";
 const dotenv = require("dotenv");
 dotenv.config( { path: path.join(root,"process.env") } );
 const port = process.env.PORT;
@@ -18,10 +19,13 @@ var userInfo = {
 var webpages = {
   main: webpageRoot+"\\"+"main.html",
   webJS: webpageRoot+"\\"+"comms.js",
-  postLogin: webpageRoot+"\\"+"login.html"
+  home: webpageRoot+"\\"+"home.html",
+  inventory:webpageRoot+"\\"+"inventory.html",
+  characters:webpageRoot+"\\"+"characters.html"
+
 };
-var privatekey = fs.readFileSync(path.join(root,"Server Files\\key.pem"));
-var certificate = fs.readFileSync(path.join(root,"Server Files\\cert.pem"));
+var privatekey = fs.readFileSync(path.join(root,"key.pem"));
+var certificate = fs.readFileSync(path.join(root,"cert.pem"));
 var credentials = {key: privatekey, cert: certificate};
 app.use(express.static(webpageRoot));
 var httpsServer = https.createServer(credentials,app);
@@ -35,7 +39,7 @@ app.get("/loggedin", function(request, response){
   userInfo.authCode = params.get("code");
   userInfo.state = params.get("state");
   console.log("loading new webpage with user content.");
-  response.sendFile(webpages.postLogin);
+  response.sendFile(webpages.home);
 })
 app.get("/login", function(request, response){
   console.log("user login requested.");
@@ -44,5 +48,18 @@ app.get("/login", function(request, response){
   url.searchParams.append("response_type","code");
   url.searchParams.append("state","1234556");
   response.redirect(url);
+});
+app.get("/home",function(request,response){
+  response.sendFile(webpages.home);
+});
+app.get("/inventory",function(request,response){
+  getUserAccessData().then(function(result){
+    console.log(result);
+    response.send(result);
+  })
+  //response.sendFile(webpages.inventory);
+});
+app.get("/characters",function(request,response){
+  response.sendFile(webpages.characters);
 });
 httpsServer.listen(port);
