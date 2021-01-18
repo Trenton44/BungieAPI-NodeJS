a5c623de8c47c2b736e7466ac84044d30a0250fb
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2021-01-18 01:05:26 -0600

    -Something broke in the github pushes, so i've wiped everything hoping to fix it

--- Server Files/ErrorResponse.js
+++
@@ -1,9 +0,0 @@
-const D2Components = require("./D2components");
-const dotenv = require("dotenv");
-const path = require("path");
-const root = path.join(__dirname,"..\\");
-dotenv.config( { path: path.join(root,"process.env") } );
-
-//I'm gonna try to do with error handling what I'm doing with D2components.js,
-//ideally so that errors are passed from anywhere in server code to be handled here.
-//And then a value is returned that can be logged.
