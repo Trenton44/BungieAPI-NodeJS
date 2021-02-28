71c111a2233218b79dcd46326298eb2b3b8b78ed
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2021-02-28 16:10:04 -0600

    Ok, i've finally got the app engine up and responding, the page now correctly server all files. This commit should make the cloud build pull the manifest files on creation.

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const bungieRoot = "https://www.bungie.net/Platform";
 const bungieCommon = "https://www.bungie.net";
 const bungieAuthURL = "https://www.bungie.net/en/OAuth/Authorize";
 const bungieTokURL = bungieRoot+"/app/oauth/token/";
+
+const D2API = require(serverRoot+"/D2API.js");
+const D2Components = require(serverRoot+"/D2Components.js");
+const ServerResponse = require(serverRoot+"/Server_Responses.js");
+const D2Responses = require(serverRoot+"/D2APIResponseObjects.js");
+
 app.set('trust proxy', true);
 app.use(express.json());
 app.use("/assets", express.static('Asset_Files'));
